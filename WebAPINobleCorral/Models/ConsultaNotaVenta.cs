57a7c8f067a242e8e01f84575384b1b98a886375
jose <ceartunduaga@gmail.com>
2020-05-27 17:22:40 -0400

    cambios n lectura Agregdo lecura de respuesta de error

--- WebAPINobleCorral/Models/ConsultaNotaVenta.cs
+++ WebAPINobleCorral/Models/ConsultaNotaVenta.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -29,5 +31,64 @@ namespace WebAPINobleCorral.Models
         public bool status { get; set; }
         public int code { get; set; }
         public string message { get; set; }
+    }
+
+    public class GenerarDocumento
+    {
+        public string empresa { get; set; }
+        public string tipodocto { get; set; }
+        public string Numero { get; set; }
+        public string iTimeOut { get; set; }
+    }
+
+    public class RespuestaDocNoEncontrado
+    {
+        public string tipo { get; set; }
+        public string estado { get; set; }
+        public string descripcion { get; set; }
+    }
+    public static class ObjectExtensions
+    {
+        public static IDictionary<string, string> ToKeyValue(this object metaToken)
+        {
+            if (metaToken == null)
+            {
+                return null;
+            }
+
+            JToken token = metaToken as JToken;
+            if (token == null)
+            {
+                return ToKeyValue(JObject.FromObject(metaToken));
+            }
+
+            if (token.HasValues)
+            {
+                var contentData = new Dictionary<string, string>();
+                foreach (var child in token.Children().ToList())
+                {
+                    var childContent = child.ToKeyValue();
+                    if (childContent != null)
+                    {
+                        contentData = contentData.Concat(childContent)
+                            .ToDictionary(k => k.Key, v => v.Value);
+                    }
+                }
+
+                return contentData;
+            }
+
+            var jValue = token as JValue;
+            if (jValue?.Value == null)
+            {
+                return null;
+            }
+
+            var value = jValue?.Type == JTokenType.Date ?
+                jValue?.ToString("o", CultureInfo.InvariantCulture) :
+                jValue?.ToString(CultureInfo.InvariantCulture);
+
+            return new Dictionary<string, string> { { token.Path, value } };
+        }
     }
 }
--- WebAPINobleCorral/Services/ConsultaNotaVenta.cs
+++ WebAPINobleCorral/Services/ConsultaNotaVenta.cs
@@ -6,13 +6,19 @@ using System.Linq;
 using System.Xml;
 using NLog;
 using Newtonsoft.Json;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+using System.Xml.Serialization;
+using System.Xml.Linq;
 
 namespace WebAPINobleCorral.Services
 {
     public class ConsultaNotaVenta
     {
         private static readonly NLog.ILogger log = NLog.LogManager.GetCurrentClassLogger();
-        
+        private static readonly HttpClient client = new HttpClient();
+
         public ResponseNotaventa ConsultarNota()
         {
             ResponseNotaventa response = new ResponseNotaventa();
@@ -20,9 +26,19 @@ namespace WebAPINobleCorral.Services
             try
             {
                 XmlDocument xDoc = new XmlDocument();
-                xDoc.Load("c:/ConsultaDocumentos.xml");
+                xDoc.Load("c:/Users/Chicho/Documents/totalpack/proyectos/noble corral/Consulta Documentos/prueba.xml");
                 var doc = xDoc.InnerXml;
                 var xml = System.Xml.Linq.XElement.Parse(doc);
+
+                var dataEncabezado4 = (from el in xml.Descendants("MENSAJE")
+                                      select new RespuestaDocNoEncontrado()
+                                      {
+                                          tipo = (string)el.Element("TIPO") ?? "",
+                                          estado = (string)el.Element("ESTADO") ?? "",
+                                          descripcion = (string)el.Element("DESCRIPCION") ?? ""
+                                      }).ToArray();
+
+
                 var dataEncabezado = (from el in xml.Descendants("ENCABEZADO")
                                       select new ConsultaNVCabezera()
                                       {
@@ -35,14 +51,14 @@ namespace WebAPINobleCorral.Services
                 var dataDetalle = (from el in xml.Descendants("DETALLE")
                                    select new ConsultaNVDetalle()
                                    {
-                                       producto     = (string)el.Element("Producto") ?? "",
-                                       cantidad     = (string)el.Element("Cantidad") ?? "",
-                                       totalProd    = (string)el.Element("Total") ?? "",
+                                       producto = (string)el.Element("Producto") ?? "",
+                                       cantidad = (string)el.Element("Cantidad") ?? "",
+                                       totalProd = (string)el.Element("Total") ?? "",
                                    }).ToArray();
 
                 var num = dataDetalle.Count();
 
-                if (dataEncabezado !=null)
+                if (dataEncabezado != null)
                 {
                     log.Info("R: Nota de venta: " + JsonConvert.SerializeObject(dataEncabezado));
                     response.dataC = dataEncabezado;
@@ -73,6 +89,51 @@ namespace WebAPINobleCorral.Services
                 return response;
             }
 
+        }
+
+        public async Task<RespuestaDocNoEncontrado> RealizarConsulta()
+        {
+            //var url = string.Format("{0}/vitacora", Settings.Default.UrlApi);
+            GenerarDocumento documento = new GenerarDocumento();
+            var url = "http://200.72.199.83//Ws%20Documento/Documento.asmx/GeneraDocumento";
+            documento.empresa = "E01";
+            documento.tipodocto = "NOTA DE VENTA";
+            documento.Numero = "0000000085";
+            documento.iTimeOut = "30";
+            var keyValues = documento.ToKeyValue();
+
+            var nvc = new List<KeyValuePair<string, string>>();
+            var req = new HttpRequestMessage(HttpMethod.Post, url) { Content = new FormUrlEncodedContent(keyValues) };
+
+            try
+            {
+                using (var response = await client.SendAsync(req))
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    content = content.Replace("&lt;", "<");
+                    content = content.Replace("&gt;", ">");
+                    var xml = System.Xml.Linq.XElement.Parse(content);
+                    XNamespace ns = "http://tempuri.org/";
+                    var dataEncabezado4 = (from el in xml.Descendants(ns + "MENSAJE")
+                                           select new RespuestaDocNoEncontrado()
+                                           {
+                                               tipo = (string)el.Element(ns + "TIPO") ?? "",
+                                               estado = (string)el.Element(ns + "ESTADO") ?? "",
+                                               descripcion = (string)el.Element(ns + "DESCRIPCION") ?? ""
+                                           }).ToArray();
+                    log.Info(content);
+                    RespuestaDocNoEncontrado response2 = new RespuestaDocNoEncontrado();
+                    return response2;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                log.Info(ex);
+            }
+            RespuestaDocNoEncontrado response3 = new RespuestaDocNoEncontrado();
+            return response3;
         }
     }
 }
